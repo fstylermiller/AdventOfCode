9c18b7558ab6cb6ef84767eac163bd0c067f1225
miller <fstylermiller@gmail.com>
2025-11-30 00:35:55 -0800

    Got an answer, is it correct? nah

--- year/2022/day/16/part/one.js
+++ year/2022/day/16/part/one.js
@@ -7,7 +7,7 @@ const volcano = raw.split("\n").reduce((acc, row) => {
   return {
     ...acc,
     [words[1]]: {
-      rate: words[4].split("=")[1].split(";")[0],
+      rate: new Number(words[4].split("=")[1].split(";")[0]),
       connections: words.slice(9).map((connection) => connection.split(",")[0]),
       isOpen: false,
     },
@@ -30,24 +30,93 @@ const targets = Object.entries(volcano).reduce((acc, [name, meta]) => {
 // determine whether in the same amount of minutes multiple other targets could be reached and opened.
 // choose the better of the two options
 
-const navigate = (volcano, startPosition) => {
-  // startPosition = "AA";
-  let currentPosition = startPosition;
-  const valveCandidates = findTargetDepths(volcano, currentPosition);
-  console.log("Candidates from current position", valveCandidates);
-  // sort and collapse valve candidates to identify the initial path.
-  // traverse the path, counting minutes used
-  // recurse from the new position until minutes used = 30
+const navigate = (volcano, startPosition, remainingTime, pressure) => {
+  const options = findTargetDepths(volcano, startPosition, remainingTime);
+  const target = determineTarget(options, remainingTime);
+  const {
+    name: nextPosition,
+    timeCost,
+    releasedPressure,
+    valvesOpened,
+  } = target;
+
+  if (!nextPosition) {
+    return pressure;
+  }
+
+  remainingTime -= timeCost;
+  pressure += releasedPressure;
+
+  valvesOpened.forEach((valve) => {
+    volcano[valve].isOpen = true;
+    delete targets[valve];
+  });
+
+  if (remainingTime > 0) {
+    return navigate(volcano, nextPosition, remainingTime, pressure);
+  }
+
+  return pressure;
 };
 
-const findTargetDepths = (volcano, startPosition) => {
+// Given a map of target valves and directions to get there
+// determine the best possible route (weighted)
+const determineTarget = (targets, remainingTime) => {
+  const pathValues = {};
+  let releasedPressure = 0;
+
+  Object.entries(targets).forEach(([name, meta]) => {
+    let timeCost = 0;
+    let valvesOpened = [];
+
+    const { pathTaken, rate: finalValveRate } = meta;
+
+    for (valve of pathTaken) {
+      const { rate, isOpen } = volcano[valve];
+
+      // If the valve in the path is not open, and has a nonzero flow rate, take an extra minute to open it.
+      if (!isOpen && rate > 0 && remainingTime - (timeCost + 2) > 0) {
+        timeCost += 2;
+        releasedPressure += rate * (remainingTime - timeCost);
+        valvesOpened.push[valve];
+      } else {
+        timeCost++;
+      }
+    }
+
+    timeCost += 2;
+    releasedPressure = finalValveRate * (remainingTime - timeCost);
+
+    pathValues[name] = {
+      name,
+      valvesOpened: [...valvesOpened, name],
+      weight: releasedPressure / timeCost,
+      releasedPressure,
+      timeCost,
+    };
+  });
+
+  return Object.keys(pathValues).reduce(
+    (acc, path) => {
+      return acc.weight < pathValues[path].weight ? pathValues[path] : acc;
+    },
+    { weight: -1 }
+  );
+};
+
+/**
+ * Finds paths to all remaining targets from a given starting position
+ * @param {*} volcano A map of valves and their connections
+ * @param {*} startPosition The starting position
+ * @param {*} initialTime The amount of time left (maximum depth possible)
+ * @returns A modified map of targets that includes directions to it (pathTaken)
+ */
+const findTargetDepths = (volcano, startPosition, remainingTime) => {
   const toVisit = [{ name: startPosition, depth: 0, pathTaken: [] }];
   const visited = new Set();
   const result = {};
-  let numberOfSteps = 0;
 
   while (toVisit.length) {
-    numberOfSteps++;
     const currentPosition = toVisit.shift();
     const {
       name: currentValve,
@@ -55,7 +124,11 @@ const findTargetDepths = (volcano, startPosition) => {
       pathTaken: currentPath,
     } = currentPosition;
 
-    if (!visited.has(currentValve)) {
+    if (
+      !visited.has(currentValve) &&
+      // potential off by one here
+      remainingTime - currentPath.length > 0
+    ) {
       visited.add(currentValve);
 
       if (targets[currentValve] && !targets[currentValve].isOpen) {
@@ -79,26 +152,11 @@ const findTargetDepths = (volcano, startPosition) => {
   return result;
 };
 
-// console.log("targets", targets);
-navigate(volcano, "AA");
+const startPosition = "AA";
+const timeRemaining = 30;
+const pressure = 0;
 
-// const bfs = (volcano, start) => {
-//   const toVisit = [start];
-//   const visited = new Set();
-//   const result = [];
+const bestPressure = navigate(volcano, startPosition, timeRemaining, pressure);
 
-//   while (toVisit.length) {
-//     const valve = toVisit.shift();
-//     if (!visited.has(valve)) {
-//       visited.add(valve);
-//       result.push(valve);
-
-//       const { connections } = volcano[valve];
-//       for (const neighbor of connections) {
-//         toVisit.push(neighbor);
-//       }
-//     }
-//   }
-
-//   return result;
-// };
+console.log(`released ${bestPressure} psi`);
+console.log(`remainingTargets:`, targets);
