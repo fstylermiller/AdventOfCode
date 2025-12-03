92723c1d19700b1fd021e401b225f06b661da801
Tyler Miller <fstylermiller@gmail.com>
2025-12-02 23:41:17 -0800

    day three part two done

---
+++ year/2025/day/3/part/two.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+
+const raw = fs.readFileSync(__dirname + "/../part/input.txt").toString();
+
+const stringBanks = raw.split("\n");
+const banks = stringBanks.map((row) => row.split("").map((str) => new Number(str)));
+
+const calculateJoltz = () => {
+  let totalJoltz = 0;
+
+  for (bank of banks) {
+    const countedJoltz = new Number(getMaxBank(bank, 12));
+    totalJoltz += countedJoltz;
+  }
+
+  return totalJoltz;
+}
+
+const getMaxBank = (bank, remainingDigits) => {
+  // find largest number and it's index
+  const biggestJolt = bank.slice(0, bank.length - (remainingDigits - 1)).reduce((acc, jolt) => acc < jolt ? jolt : acc, 0);
+  const joltIndex = bank.indexOf(biggestJolt);
+
+  if (remainingDigits === 1) {
+    return `${biggestJolt}`;
+  }
+
+  return `${biggestJolt}` + getMaxBank(bank.slice(joltIndex + 1), remainingDigits - 1);
+};
+
+const gigaJoltz = calculateJoltz();
+
+console.log(gigaJoltz);
+
