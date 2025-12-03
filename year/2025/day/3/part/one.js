698c203556e562bdf8766e91d16e6e1ee53b7e2e
Tyler Miller <fstylermiller@gmail.com>
2025-12-02 22:54:52 -0800

    part one of day three done

---
+++ year/2025/day/3/part/one.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+
+const raw = fs.readFileSync(__dirname + "/../part/input.txt").toString();
+
+const stringBanks = raw.split("\n");
+const banks = stringBanks.map((row) => row.split("").map((str) => new Number(str)));
+
+// const calculateJolts = (biggest, distance) => {
+//   // find the biggest digit that is n distance from the end
+//   return calculateJolts(newBiggest, distance);
+// }
+
+let totalJoltz = 0;
+
+banks.forEach(row => {
+  let maxFirstDigit = 0;
+  let firstDigitPos = 0;
+  for (let i = 0; i < row.length - 1; i++) {
+    if (row[i] > maxFirstDigit) {
+      maxFirstDigit = row[i]
+      firstDigitPos = i;
+    }
+  }
+
+  let maxSecondDigit = 0;
+  for (let i = firstDigitPos + 1; i <row.length; i++) {
+    if (row[i] > maxSecondDigit) {
+      maxSecondDigit = row[i]
+    }
+  }
+  
+  totalJoltz += maxFirstDigit * 10 + maxSecondDigit
+})
+
+
+console.log(totalJoltz);
