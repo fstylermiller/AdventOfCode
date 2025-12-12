3cbfeac27995a0b1e2b4c6bc68d9a11f219d1ce9
miller <fstylermiller@gmail.com>
2025-12-12 08:56:42 -0800

    got day 6 part one

---
+++ year/2025/day/6/part/one.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+
+const raw = fs.readFileSync(__dirname + "/test-input.txt").toString();
+
+const doCephalopodHomework = () => {
+  const isAddition = {
+    "*": false,
+    "+": true,
+  };
+
+  const problems = raw
+    .split("\n")
+    .map((row) => row.split(" ").filter((column) => column !== ""));
+
+  let answer = 0;
+
+  for (let x = 0; x < problems[0].length; x++) {
+    const operator = problems[problems.length - 1][x];
+    let problemResult = isAddition[operator] ? 0 : 1;
+
+    for (let y = problems.length - 2; y >= 0; y--) {
+      const number = new Number(problems[y][x]);
+
+      if (isAddition[operator]) {
+        problemResult += number;
+      } else {
+        problemResult *= number;
+      }
+    }
+
+    answer += problemResult;
+  }
+
+  return answer;
+};
+
+const finalAnswer = doCephalopodHomework();
+
+console.log(`homework answer = ${finalAnswer}`);
